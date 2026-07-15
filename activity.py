8d980ce780a3b378852cb65db14ca1c03e926e95
unknown <jishnu4211@gmail.com>
2026-07-15 11:09:01 +0530

    activity

---
+++ activity.py
@@ -0,0 +1,76 @@
+#print the first element..
+# numbers = [10 ,20 ,30 , 40 , 50]
+# print(numbers[0])    # 10
+
+# print the last element..
+# print(numbers[4])    # 50
+
+# print the third element ..
+# print(numbers[2])   # 30
+
+# print the first three element..
+# print(numbers[0 : 3])      
+ 
+
+ # print the last two elements ..
+
+# print(numbers[-2 :])
+
+#reverse the list using slicing ..
+
+# print(numbers[:: -1])
+
+# find the length of the string ..
+# print (len(numbers))
+
+# find the maximum number 
+# print(max(numbers))
+
+# find the minimum number 
+# print(min(numbers))
+
+#
+
+#level 2
+
+# fruits =[ "Apple" ,"Banana" , "Orange"]
+
+
+# add mango to the end
+
+# fruits.insert(3,"Mango")
+# print(fruits)
+
+# insert grapes at index 1..
+# fruits.insert(1,"Grapes")
+# print(fruits)
+
+# remove banana 
+# fruits.remove("Banana")
+# print(fruits)
+
+# remove the last item 
+# fruits.pop()
+# print(fruits)
+
+# clear the entire list ..
+# print(fruits[::] )
+
+#find the index of "orange"..
+# print(fruits.index("Orange"))
+
+# given
+# fruits =[ "Apple" ,"Banana" , "Apple" ,"Orange"]
+
+# given 
+# [45 ,10, 78,22, 5]
+
+# num=[45 ,10, 78,22, 5]
+
+# num.sort(reverse=True)
+# print(num)
+
+
+# 
+
+
---
+++ index.py
@@ -0,0 +1,32 @@
+# Creating Sets
+# my_set ={1,2,3,4,5}
+# mixed_set={1,'hello',3.14}
+# empty_set=set() # creates dict , not set
+# from_list =set([1,2,2,3]) # {1,2,3}
+
+# my_set={1,"hello",20,2,True,3,2,40,5,14}
+# print(my_set)
+
+#set _Method
+#------------
+#add(item)-Add single element
+# my_set = {1, 2, 3, 4, 5}
+# my_set.add(100)
+# print(my_set)
+
+#2 ) remove(item) -Remove element (error if not found)
+# my_set = {1, 2, 3, 4, 5}
+# my_set.remove(500)
+# print(my_set)
+
+
+# 3 ) discard(item) -Remove element (no error)
+# my_set ={1, 2, 3, 4, 5}
+# my_set.discard(400)
+# print(my_set)
+
+# 4) union(set2) - Combine two sets
+# my_set1= {1, 2 , 3 ,4,5}
+# my_set2={10,20,30,40,50}
+# new_set=my_set1.union(my_set2)
+# print(new_set)
